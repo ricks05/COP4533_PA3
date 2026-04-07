2330214aa050fc7fcc4439781ef29bb39f100a41
Rick Silva <hbsilva2005@gmail.com>
2026-04-06 20:03:06 -0400

    finished dp algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
 #include <fstream>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
@@ -22,8 +25,54 @@ int main() {
     string A, B;
     input >> A;
     input >> B;
+    A = '#' + A;  // filler character so that indexed accesses are nicer
+    B = '#' + B;  // "
 
     input.close();
+
+    // algorithm
+    size_t m = A.size();
+    size_t n = B.size();
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+
+    for (int i = 1; i < m; i++) {
+        for (int j = 1; j < n; j++) {
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            if (A[i] == B[j]) {
+                dp[i][j] = max(dp[i][j], dp[i-1][j-1] + value[A[i]]);
+            }
+        }
+    }
+
+    ofstream output("../io/example.out");
+    if (!output) {
+        cerr << "Could not open file example.out";
+        return 1;
+    }
+
+    output << dp[m-1][n-1] << endl;
+
+    // backtracking
+    string res;
+    int i = m - 1;
+    int j = n - 1;
+    while (i > 0 && j > 0) {
+        if (A[i] == B[j] && dp[i][j] == dp[i-1][j-1] + value[A[i]]) {
+            res += A[i];
+            i -= 1;
+            j -= 1;
+        }
+        else if (dp[i][j] == dp[i-1][j]) {
+            i -= 1;
+        }
+        else {
+            j -= 1;
+        }
+    }
+
+    reverse(res.begin(), res.end());
+    output << res;
+    output.close();
 
     return 0;
 }
